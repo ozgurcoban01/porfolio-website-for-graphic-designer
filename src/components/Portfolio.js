4f4ef1751dba350c2f2fdb84f892a2df1c1d1386
ozgurcoban01 <102681848+ozgurcoban01@users.noreply.github.com>
2023-03-15 09:49:12 +0300

    ready for other pages

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,71 +4,113 @@ import Content from "./Content.js";
 import PortfolioContent from "./PortfolioContent.js";
 import Navbar from "./Navbar.js";
 
+import d3dboothdesign from "../assets/portfolio/d3dboothdesign.jpeg";
+import alisverisfestivali from "../assets/portfolio/alisverisfestivali.jpg";
+import alisverisfestivaliv from "../assets/portfolio/alisverisfestivali.mp4";
+import buldingcladding from "../assets/portfolio/buldingcladding.jpg";
+import carraffle from "../assets/portfolio/carraffle.jpg";
+import carrafflestory from "../assets/portfolio/carrafflestory.jpg";
+import carrafflestoryv from "../assets/portfolio/carrafflestory.mp4";
+import efsanekasimbasliyor from "../assets/portfolio/efsanekasimbasliyor.jpg";
+import efsanekasimbasliyorv from "../assets/portfolio/efsanekasimbasliyor.mp4";
+import gamecoupondesign from "../assets/portfolio/gamecoupondesign.jpeg";
+import instagrampostfestival from "../assets/portfolio/instagrampostfestival.jpeg";
+import instagramreels from "../assets/portfolio/instagramreels.jpg";
+import instagramreelsv from "../assets/portfolio/instagramreels.mp4";
+import instagramstoryproduct from "../assets/portfolio/instagramstoryproduct.jpeg";
+import instagramstoryvalentineday from "../assets/portfolio/instagramstoryvalentineday.jpeg";
+import metrobusdressing from "../assets/portfolio/metrobusdressing.jpg";
+import nescafegold from "../assets/portfolio/nescafegold.jpg";
+import nescafegoldv from "../assets/portfolio/nescafegold.mp4";
+import newyear from "../assets/portfolio/newyear.jpg";
+import newyearv from "../assets/portfolio/newyear.mp4";
+import raffleinstagrampost from "../assets/portfolio/raffleinstagrampost.jpeg";
+import rafflescooterpost from "../assets/portfolio/rafflescooterpost.jpeg";
+import remembranceday from "../assets/portfolio/remembranceday.jpg";
+import remembrancedayv from "../assets/portfolio/remembranceday.mp4";
+import valentineday from "../assets/portfolio/valentineday.jpeg";
+import victoryday from "../assets/portfolio/victoryday.jpg";
+import victorydayv from "../assets/portfolio/victoryday.mp4";
+
 const Portfolio = () => {
  
   const popHupContents = [
     {
-      intext: "1",
-      image: `https://source.unsplash.com/random/`,
-    },
-    {
-      intext: "2",
-      image: `https://source.unsplash.com/random/`,
-    },
-    {
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "3D Booth Design",
+      image: d3dboothdesign,
+      video: d3dboothdesign,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "Alışveriş Festivali",
+      image: alisverisfestivali,
+      video: alisverisfestivaliv,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "Building Cladding",
+      image: buldingcladding,
+      video: buldingcladding,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "Car Raffle",
+      image: carraffle,
+      video: carraffle,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "Car Raffle Story",
+      image: carrafflestory,
+      video: carrafflestoryv,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "Efsane Kasım Başlıyor",
+      image: efsanekasimbasliyor,
+      video: efsanekasimbasliyorv,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "Game Coupon Desing",
+      image: gamecoupondesign,
+      video: gamecoupondesign,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "Instagram Post Festival",
+      image: instagrampostfestival,
+      video: instagrampostfestival,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "Instagram Reels",
+      image: instagramreels,
+      video: instagramreelsv,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "Instagram Story Product",
+      image: instagramstoryproduct,
+      video: instagramstoryproduct,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "Instagram Valentine Day",
+      image: instagramstoryvalentineday,
+      video: instagramstoryvalentineday,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "Metrobus Dressing",
+      image: metrobusdressing,
+      video: metrobusdressing,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "Nescafe Gold",
+      image: nescafegold,
+      video: nescafegoldv,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "New Year",
+      image: newyear,
+      video: newyearv,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "Instagram Raffle Post",
+      image: raffleinstagrampost,
+      video: raffleinstagrampost,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "Raffle Scooter",
+      image: rafflescooterpost,
+      video: rafflescooterpost,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "Remembrance Day",
+      image: remembranceday,
+      video: remembrancedayv,
     },{
-      intext: "3",
-      image: `https://source.unsplash.com/random/`,
+      intext: "Valentine Day",
+      image: valentineday,
+      video: valentineday,
+    },{
+      intext: "Victory Day",
+      image: victoryday,
+      video: victorydayv,
     },
   ];
 
@@ -77,9 +119,9 @@ const Portfolio = () => {
       <Navbar />
       <div className="portfolioContainer ">
         <div className="portfolioPageLayer1">
-          <span className="line"></span>
+     
           <div className="portfolioText">Portfolio</div>
-          <span className="line"></span>
+
         </div>
 
         <div className="portfolioContentContainer">
--- src/components/PortfolioContent.js
+++ src/components/PortfolioContent.js
@@ -4,18 +4,28 @@ import Content from "./Content.js";
 import Navbar from "./Navbar.js";
 
 const PortfolioContent = (props) => {
-    const [pophupContent, setPopHupContent] = useState(false);
+  const [pophupContent, setPopHupContent] = useState(false);
 
-    const pictureURL=props.data.image+Math.floor(Math.random() * 100);
+  const pictureURL = props.data.image;
+  const pictureVideo = props.data.video;
+  const urlCheck = pictureVideo.slice(-1);
+  console.log(urlCheck);
 
-    
   return (
     <>
       <div className={`popHup ${pophupContent ? "pophupContentActive" : ""}`}>
         <div onClick={() => setPopHupContent(false)} className="exit">
           X
         </div>
-        <div className="pophupContent">{props.data.intext}</div>
+        <div className="pophupContent">
+          {urlCheck == 4 ? (
+            <video controls>
+              <source src={pictureVideo} type="video/mp4"></source>
+            </video>
+          ) : (
+            <img src={pictureURL}></img>
+          )}
+        </div>
       </div>
       <div onClick={() => setPopHupContent(true)} className="portfolioContent">
         <img src={pictureURL}></img>
